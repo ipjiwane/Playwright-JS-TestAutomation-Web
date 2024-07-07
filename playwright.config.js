8ccead8c969003af91a25185f4679630915cd809
ishan <ishan@test.com>
2024-07-08 00:04:49 +0200

    updated config file

--- playwright.config.js
+++ playwright.config.js
@@ -33,8 +33,8 @@ module.exports = defineConfig({
   workers: process.env.CI ? 1 : 1,
 
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: process.env.CI ? [['html'],['allure-playwright']] : [['html'],['allure-playwright']],
-  reporter: [['html'],['allure-playwright']],
+  reporter: process.env.CI ? [['html'],['allure-playwright']] : [['html'],['allure-playwright']],
+  // reporter: [['html'],['allure-playwright']],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
